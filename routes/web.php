ed459bef124c15b6b4253e2d541c4e19aeebfd09
muna <victorycollins31@gmail.com>
2024-08-29 13:46:30 +0100

    update; cretaed listing route in web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest listings',
+        'listings' => [
+            'id' =>1,
+            'title' => 'Listing One',
+            'description' => ''
+        ]
+
+    ]);
 });
